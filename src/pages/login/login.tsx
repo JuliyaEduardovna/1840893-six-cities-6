70d0f175b69b1d4b5dec779e333be5eab16b5e66
JuliyaEduardovna <juliya.eduardovna@gmail.com>
2026-04-07 16:19:24 +0700

    7.12. Добро пожаловать, или посторонним вход воспрещён (часть 1)

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,11 +1,42 @@
+import { FormEvent, useRef } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { login } from '../../store/user-actions';
+import { AppRoutes } from '../../constants/constants';
+import { AppDispatch } from '../../store';
+
 export default function Login(): JSX.Element {
+  const dispatch = useDispatch<AppDispatch>();
+  const navigate = useNavigate();
+  const loginRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+
+    if (loginRef.current && passwordRef.current) {
+      const email = loginRef.current.value;
+      const password = passwordRef.current.value.trim();
+
+      if (password.length === 0) {
+        return;
+      }
+
+      dispatch(login({ email, password })).then((result) => {
+        if (result.meta.requestStatus === 'fulfilled') {
+          navigate(AppRoutes.Root);
+        }
+      });
+    }
+  };
+
   return (
     <div className="page page--gray page--login">
       <header className="header">
         <div className="container">
           <div className="header__wrapper">
             <div className="header__left">
-              <a className="header__logo-link" href="main.html">
+              <Link className="header__logo-link" to={AppRoutes.Root}>
                 <img
                   className="header__logo"
                   src="img/logo.svg"
@@ -13,7 +44,7 @@ export default function Login(): JSX.Element {
                   width={81}
                   height={41}
                 />
-              </a>
+              </Link>
             </div>
           </div>
         </div>
@@ -22,10 +53,11 @@ export default function Login(): JSX.Element {
         <div className="page__login-container container">
           <section className="login">
             <h1 className="login__title">Sign in</h1>
-            <form className="login__form form" action="#" method="post">
+            <form className="login__form form" onSubmit={handleSubmit}>
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">E-mail</label>
                 <input
+                  ref={loginRef}
                   className="login__input form__input"
                   type="email"
                   name="email"
@@ -36,6 +68,7 @@ export default function Login(): JSX.Element {
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">Password</label>
                 <input
+                  ref={passwordRef}
                   className="login__input form__input"
                   type="password"
                   name="password"
@@ -53,9 +86,9 @@ export default function Login(): JSX.Element {
           </section>
           <section className="locations locations--login locations--current">
             <div className="locations__item">
-              <a className="locations__item-link" href="#">
+              <Link className="locations__item-link" to={AppRoutes.Root}>
                 <span>Amsterdam</span>
-              </a>
+              </Link>
             </div>
           </section>
         </div>
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,30 @@
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosError, AxiosInstance } from 'axios';
 import { API_BASE_URL, API_TIMEOUT } from '../constants/constants';
+import { getToken, removeToken } from './token';
 
 export const createAPI = (): AxiosInstance => {
   const api = axios.create({
     baseURL: API_BASE_URL,
     timeout: API_TIMEOUT,
   });
+
+  api.interceptors.request.use((config) => {
+    const token = getToken();
+    if (token && config.headers) {
+      config.headers['x-token'] = token;
+    }
+    return config;
+  });
+
+  api.interceptors.response.use(
+    (response) => response,
+    (error: AxiosError) => {
+      if (error.response?.status === 401) {
+        removeToken();
+      }
+      throw error;
+    }
+  );
 
   return api;
 };
